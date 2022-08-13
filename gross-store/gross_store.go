ce7dd3aa6b7c681dfecaa8e7a58896ef7b6c5269
Roger Almeida <roger.eduardo@gmail.com>
2022-08-13 12:42:46 +1000

    :sparkles: gross store final solution

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -2,14 +2,14 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	myMap := map[string]int{}
-	myMap["quarter_of_a_dozen"] = 3
-	myMap["half_of_a_dozen"] = 6
-	myMap["dozen"] = 12
-	myMap["small_gross"] = 120
-	myMap["gross"] = 144
-	myMap["great_gross"] = 1728
-	return myMap
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
 }
 
 // NewBill creates a new bill.
@@ -19,15 +19,35 @@ func NewBill() map[string]int {
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	return false
+	quantity, exists := units[unit]
+	if exists {
+		bill[item] += quantity
+	}
+	return exists
+}
+
+func cannotRemoveItem(doesItemExistInBill, doesUnitExistInUnits bool, newQuantity int) bool {
+	return !doesItemExistInBill || !doesUnitExistInUnits || newQuantity < 0
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	return false
+	itemQuantity, doesItemExistInBill := bill[item]
+	unitQuantity, doesUnitExistInUnits := units[unit]
+	newQuantity := itemQuantity - unitQuantity
+	if cannotRemoveItem(doesItemExistInBill, doesUnitExistInUnits, newQuantity) {
+		return false
+	}
+	if newQuantity == 0 {
+		delete(bill, item)
+	} else {
+		bill[item] = newQuantity
+	}
+	return true
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	return 0, false
+	qtd, exists := bill[item]
+	return qtd, exists
 }
